4b3f0f07979e6c103c8afcf4b768cd910fd45ef9
Beatriz Jardim <paulabeatrizlj@hotmail.com>
2023-04-13 22:00:52 -0300

    implementação envolvendo a extensão .fasta

--- big-data-mapreduce-hadoop3-student-master/src/advanced/entropy/EntropyFASTA.java
+++ big-data-mapreduce-hadoop3-student-master/src/advanced/entropy/EntropyFASTA.java
@@ -31,18 +31,75 @@ public class EntropyFASTA {
         // arquivo de saida
         Path output = new Path(files[1]);
 
+        //job 1
+        Job j1 = new Job(c, "etapa 1");
+
+        // registro de classe
+        j1.setJarByClass(EntropyFASTA.class);
+        j1.setMapperClass(MapEtapaA.class);
+        j1.setReducerClass(ReduceEtapaA.class);
+        //Definição do tipos de saida
+        j1.setMapOutputKeyClass(Text.class);
+        j1.setMapOutputValueClass(LongWritable.class);
+        j1.setOutputKeyClass(Text.class);
+        j1.setOutputValueClass(LongWritable.class);
+        //Definir os arquivos de entrada e saida
+        FileInputFormat.addInputPath(j1, input);
+        FileOutputFormat.setOutputPath(j1, intermediate);
+
+        //rodando o job 1
+        j1.waitForCompletion(false);
+        //in/nomearquivo outpt/entropia.txt
+
+
+        //job 2
+        Job j2 = new Job(c, "etapa 2");
+        // registro de classe
+        j2.setJarByClass(EntropyFASTA.class);
+        j2.setMapperClass(MapEtapaB.class);
+        j2.setReducerClass(ReduceEtapaB.class);
+        //Definição do tipos de saida
+        j2.setMapOutputKeyClass(Text.class);
+        j2.setMapOutputValueClass(LongWritable.class);
+        j2.setOutputKeyClass(Text.class);
+        j2.setOutputValueClass(LongWritable.class);
+        //Definir os arquivos de entrada e saida
+        FileInputFormat.addInputPath(j2, intermediate);
+        FileOutputFormat.setOutputPath(j2, output);
+
+        //rodando o job 2
+        j2.waitForCompletion(false);
+
     }
 
     public static class MapEtapaA extends Mapper<LongWritable, Text, Text, LongWritable> {
         public void map(LongWritable key, Text value, Context con)
                 throws IOException, InterruptedException {
+            //pegando uma linha do arquivo
+            String linha = value.toString();
+
+            //ignorando o cabeçalho
+            if(!linha.startsWith(">")) {
+                String caracteres[] = linha.split("");
+
+                //gerando (chave = caracter, valor = 1)
+
+                for (String c : caracteres) {
+                    con.write(new Text(c), new LongWritable(1));
+                }
+            }
         }
     }
 
     public static class ReduceEtapaA extends Reducer<Text, LongWritable, Text, LongWritable> {
         public void reduce(Text key, Iterable<LongWritable> values, Context con)
                 throws IOException, InterruptedException {
-
+            // somando oas ocorrencias do caracter
+            long soma = 0;
+            for(LongWritable v : values){
+                soma += v.get();
+            }
+            con.write(key, new LongWritable(soma));
         }
     }
 
@@ -50,13 +107,39 @@ public class EntropyFASTA {
     public static class MapEtapaB extends Mapper<LongWritable, Text, Text, BaseQtdWritable> {
         public void map(LongWritable key, Text value, Context con)
                 throws IOException, InterruptedException {
+            // obtendo a linh
+            String linha = value.toString();
 
+            //obtendo os campos
+            String campos[] = linha.split("/t");
+
+            // criando uma chave fixa para garantir que todos os caracteres sejam enviados por mesmo reduce
+            con.write(new Text("todos"),
+                    new BaseQtdWritable(campos[0],
+                            Long.parseLong(campos[1])));
         }
     }
 
     public static class ReduceEtapaB extends Reducer<Text, BaseQtdWritable, Text, DoubleWritable> {
         public void reduce(Text key, Iterable<BaseQtdWritable> values, Context con)
                 throws IOException, InterruptedException {
+            LinkedList<BaseQtdWritable> valores = new LinkedList<>();
+
+            long total = 0;
+
+            for(BaseQtdWritable v : values){
+                total += v.getContagem();
+                valores.add(new BaseQtdWritable(v.getCaracter(),
+                        v.getContagem()));
+            }
+
+            //calculando a probbilidade e entropia de cada caracter
+            for(BaseQtdWritable v : valores){
+                double p = v.getContagem() / (double) total;
+                double entropia = -p * Math.log(p) / Math.log(2.0);
+                con.write(new Text(v.getCaracter()),
+                        new DoubleWritable(entropia));
+            }
         }
     }
 }
